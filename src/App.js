71f18dc20fd196dfb0a7bd403f84e7051325e985
John Doe <nadia643@gmail.com>
2020-06-01 10:29:00 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>hi from app</h1>      
+    </div>
+  );
+}
+
+export default App;
