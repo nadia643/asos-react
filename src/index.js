fb008abedaab5dc0f8a1dc5c13a50cba450d07e8
John Doe <nadia643@gmail.com>
2020-06-05 10:17:07 +0100

    ammended padding to nav buttons

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1" />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
